34514719c0780e4819f065ee9f2c0245cbdd917b
jiayibing2333 <jiayibing2333@163.com>
2020-07-15 00:27:53 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dongjinglu669",
+    version="0.0.1",
+    keywords=["markdown", "Plastic-Metal"],
+    description="To generate markdown-formatted lab reports.",
+    long_description="Light-weight tool to generate markdown-formatted lab reports. For python.",
+    license="MIT",
+
+    url="https://github.com/Plastic-Metal/pyLabOn",
+    author="Ferdinand Sukhoi",
+    author_email="ferdinandsukhoi@outlook.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["pypandoc"]
+)
